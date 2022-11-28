0b5b313a9247312e327f440cd9bffbf5158ae5c7
misha-solntcev <misha.solntcev@gmail.com>
2022-11-28 21:37:37 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 18. Вычислить частичную сумму ряда S=1/1^2 +1/3^2 +1/5^2 +⋯
+Вычисление прекратить, когда сумма ряда станет больше 2.*/
+
+namespace Lab_3_1_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = 1;
+            double S = n;            
+            while (S < 1.23)
+            {
+                n += 2;
+                S += 1 / (n * n);
+                Console.WriteLine($"n={n}, S={S}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
